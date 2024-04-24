163aaf300a69a88639c71c1c7e4ee16aa7271c6e
Felipe C. de Pauli <fcdpauli@gmail.com>
2024-04-24 18:17:57 -0300

    python: structure and more Python scripts

--- PY01_Parallel/deep_study/py_02_1_single_thread.py
+++ PY01_Parallel/deep_study/py_02_1_single_thread.py
@@ -1,23 +1,42 @@
 import json
 import time
 from urllib.request import urlopen, Request
+from threading import Thread, Lock
 
-def count_letters(url, frequency):
-    response = urlopen(Request(url, headers={'User-Agent': 'Mozilla'}))
-    txt = str(response.read())
-    for l in txt:
-        letter = l.lower()
-        if letter in frequency:
-            frequency[letter] += 1
+class CountingThings():
+    def __init__(self):
+        self.frequency = {letter: 0 for letter in "abcdefghijklmnopqrstuvwxyz"}
+        self.mutex = Lock()
+        
+    def count_letters(self, url, frequency):
+        response = urlopen(Request(url, headers={'User-Agent': 'Mozilla'}))
+        txt = str(response.read())
+        for l in txt:
+            letter = l.lower()
+            if letter in frequency:
+                self.mutex.acquire()
+                frequency[letter] += 1   
+                self.mutex.release() 
+                
+    def run(self):
+        
+        initial_time = time.perf_counter()
+        
+        threads = []
+        for i in range(1000, 1020):
+            t = Thread(target=self.count_letters, args=(f"https://www.rfc-editor.org/rfc/rfc{i}.txt", self.frequency))
+            t.start()
+            threads.append(t)
+            
+        for t in threads:
+            t.join()
+            
+        final_time = time.perf_counter()
+        print(json.dumps(self.frequency, indent=4))
+        evaluate_time = final_time - initial_time
+        print(f"Spent time: {evaluate_time*1000:.5} ms")
+        my_sum = sum(list(self.frequency.values()))
+        print("Total sum:", my_sum)
 
-def main():
-    frequency = {letter: 0 for letter in "abcdefghijklmnopqrstuvwxyz"}
-    
-    initial_time = time.perf_counter()
-    for i in range(1000, 1020):
-        count_letters(f"https://www.rfc-editor.org/rfc/rfc{i}.txt", frequency)
-    final_time = time.perf_counter()
-    print(json.dumps(frequency, indent=4))
-    evaluate_time = final_time - initial_time
-    print(f"Spent time: {evaluate_time*1000:.5} ms")
-main()
+ct = CountingThings()
+ct.run()
